4fe1c1a09d2e4bc19a6eef03b52592c0cbc3d76e
Joey Piccola <joey@joeypiccola.com>
2018-07-06 16:24:56 -0600

    rc2 specs

---
+++ spec/classes/ciphers/rc2_40_128_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'crypto_win::ciphers::rc2_40_128' do
+  let :pre_condition do
+    'include ::crypto_win'
+  end
+
+  on_supported_os(facterversion: '2.4').each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'passes with minimal params' do
+        it {
+          is_expected.to contain_registry_key('rc2_40_128').with(
+            'ensure' => 'present',
+            'path'   => 'HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC2 40/128',
+          )
+        }
+
+        it {
+          is_expected.to contain_registry_value('rc2_40_128_enabled').with(
+            'ensure' => 'present',
+            'path'   => 'HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC2 40/128\Enabled',
+            'type'   => 'dword',
+            'data'   => 0,
+          )
+        }
+
+        it { is_expected.to compile }
+      end
+    end
+  end
+end
